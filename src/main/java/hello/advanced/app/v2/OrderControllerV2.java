50354293e0219b31d48fa2dc41b1613ccf888a35
MinJunChoi <chlalswns200@naver.com>
2025-01-16 14:12:13 +0900

    fix example

--- src/main/java/hello/advanced/app/v2/OrderControllerV2.java
+++ src/main/java/hello/advanced/app/v2/OrderControllerV2.java
@@ -2,6 +2,7 @@ package hello.advanced.app.v2;
 
 import hello.advanced.trace.TraceStatus;
 import hello.advanced.trace.hellotrace.HelloTraceV1;
+import hello.advanced.trace.hellotrace.HelloTraceV2;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -12,7 +13,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class OrderControllerV2 {
 
     private final OrderServiceV2 orderServiceV2;
-    private final HelloTraceV1 trace;
+    private final HelloTraceV2 trace;
 
     @GetMapping("/v2/request")
     public String request(@RequestParam("itemId") String itemId) {
